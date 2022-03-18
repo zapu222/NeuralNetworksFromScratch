e54a1ed4624eceda726a704f7447726db87529b3
Zachary Pulliam <84103628+zapu222@users.noreply.github.com>
2022-03-18 16:47:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from datasets import MNISTDataset_0_4_train, MNISTDataset_0_4_test
 ROOT = ''  # path to root folder
 arch_0_1 = os.path.join(ROOT, 'architecture_0_1.txt')  # path to architecture file 0 to 1 classification
 arch_0_4 = os.path.join(ROOT, 'architecture_0_1.txt')  # path to architecture file 0 to 4 classification 
+epochs = 10  # number of epochs to train for
 
 """ Choose between 0 to 1 or 0 to 4 classification for MNIST """
 k = 1  # set k equal to 1 or 4
@@ -25,7 +26,7 @@ def run_0_1():
     test = MNISTDataset_0_1_test(os.path.join(ROOT, 'data'))  # testing data
 
     nn = NN(arch_0_1)  # create NN
-    nn.train(train.x, train.y, 2)  # train NN
+    nn.train(train.x, train.y, epochs)  # train NN
     nn.test(test.x, test.y)  # test NN
     print('')
 
@@ -35,7 +36,7 @@ def run_0_4():
     test = MNISTDataset_0_4_test(os.path.join(ROOT, 'data'))  # testing data
 
     nn = NN(arch_0_4)  # create NN
-    nn.train(train.x, train.y, 10)  # train NN
+    nn.train(train.x, train.y, epochs)  # train NN
     nn.test(test.x, test.y)  # test NN
     print('')
 
